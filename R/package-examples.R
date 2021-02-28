166d29ecda284c4520cb74e1534f0c0ddb884aa0
Aviral Goel <goel.aviral@gmail.com>
2021-02-28 23:31:50 +0000

    Fix example extraction and add progress

--- R/package-examples.R
+++ R/package-examples.R
@@ -1,31 +1,27 @@
-#' @export
 #' @importFrom tools Rd2ex Rd_db
-extract_examples <- function(package_name,
-                             package_directory,
-                             libraries = NULL,
-                             encoding = "UTF-8",
-                             comment_dont_run = TRUE,
-                             comment_dont_test = TRUE,
-                             prepend_library_load = TRUE) {
-
-    name <- if (missing(package_name)) {
-                basename(package_directory)
-            } else {
-                package_name
-            }
+extract_examples_helper <- function(package,
+                                    libraries = NULL,
+                                    encoding = "UTF-8",
+                                    comment_dont_run = TRUE,
+                                    comment_dont_test = TRUE,
+                                    prepend_library_load = TRUE) {
 
     create_result <- function(filenames, contents) {
-        data.frame(package = name,
+        data.frame(package = if(length(filenames) == 0) character(0) else package,
                    filename = filenames,
                    content = contents)
     }
 
     db <- tryCatch({
-        Rd_db(package_name, package_directory, libraries)
+        Rd_db(package, package_directory, libraries)
     }, error = function(e) {
         print(e)
-        return(create_result(character(0), character(0)))
+        list()
     })
+
+    if(length(db) == 0) {
+        return(create_result(character(0), character(0)))
+    }
 
     example_extractor <- function(example_filename) {
         elt <- db[[example_filename]]
@@ -42,7 +38,7 @@ extract_examples <- function(package_name,
         if(nchar(code) == 0) NA_character_
 
         else if(prepend_library_load) {
-            library_load <- paste0("library", "(", name, ")")
+            library_load <- paste0("library", "(", package, ")")
             paste(library_load, code, sep = "\n")
         }
 
@@ -52,10 +48,44 @@ extract_examples <- function(package_name,
     filenames <- names(db)
     contents <- sapply(filenames, example_extractor)
 
-    na_locations <- is.na(contents)
+    na_indices <- is.na(contents)
 
-    create_result(replace_extension(filenames[!na_locations], ".R"),
-                  contents[!na_locations])
+    create_result(replace_extension(filenames[!na_indices], ".R"),
+                  contents[!na_indices])
+
+}
+
+#' @export
+#' @importFrom progress progress_bar
+extract_examples <- function(packages,
+                             libraries = NULL,
+                             encoding = "UTF-8",
+                             comment_dont_run = TRUE,
+                             comment_dont_test = TRUE,
+                             prepend_library_load = TRUE,
+                             verbose = TRUE) {
+
+    if(verbose) {
+        pb <- progress_bar$new(
+                               format = "Processing :what [:bar] :current/:total (:percent) eta: :eta",
+                               total = length(packages),
+                               width = 80)
+    }
+
+    helper  <- function(package, ...) {
+        if(verbose) pb$tick(tokens = list(what = pad(package, 15)))
+        extract_examples_helper(package, ...)
+    }
+
+    dfs <- lapply(packages,
+                  helper,
+                  libraries,
+                  encoding,
+                  comment_dont_run,
+                  comment_dont_test,
+                  prepend_library_load)
+
+    do.call(rbind, dfs)
 }
 
 #' @export
@@ -79,42 +109,4 @@ write_examples <- function(examples, output_dirpath) {
     examples$filepath <- sapply(1:nrow(examples), example_writer)
 
     examples
-}
-
-
-extract_package_examples <- function(pkg, pkg_dir, output_dir) {
-    db <- tryCatch({
-        tools::Rd_db(basename(pkg_dir), dir=dirname(pkg_dir))
-    }, error=function(e) {
-        c()
-    })
-
-    if (!length(db)) {
-        return(character())
-    }
-
-    files <- names(db)
-
-    examples <- sapply(files, function(x) {
-        f <- file.path(output_dir, paste0(basename(x), ".R"))
-        tools::Rd2ex(db[[x]], f, defines=NULL,
-                     commentDontrun=TRUE, commentDonttest=TRUE)
-
-        if (!file.exists(f)) {
-            message("Rd file `", x, "' does not contain any code to be run")
-            NA
-        } else {
-                                        # prepend the file with library call
-            txt <- c(
-                paste0("library(", pkg, ")"),
-                "",
-                "",
-                readLines(f)
-            )
-            writeLines(txt, f)
-            f
-        }
-    })
-
-    na.omit(examples)
 }
--- R/utilities.R
+++ R/utilities.R
@@ -1,6 +1,26 @@
 
 #' @importFrom tools file_path_sans_ext
 replace_extension <- function(paths, extension) {
-    paths <- file_path_sans_ext(paths)
-    paste(paths, extension, sep = "")
+    if(length(paths) == 0) {
+        character(0)
+    }
+    else {
+        paths <- file_path_sans_ext(paths)
+        paste(paths, extension, sep = "")
+    }
+}
+
+
+pad <- function(str, max, char = "", truncate = FALSE) {
+    n <- nchar(str)
+
+    if (n == max) str
+    else if (n > max) {
+        if(truncate) substr(str, 1, max)
+        else str
+    }
+    else {
+        spaces <- paste0(replicate(max - n, " "), collapse="")
+        paste0(str, spaces, collapse = "", sep = "")
+    }
 }
