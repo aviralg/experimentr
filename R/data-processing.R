4e1b10a13a1a52ec0ab5bf5d058e526862e70afc
Aviral Goel <goel.aviral@gmail.com>
2021-03-05 11:47:46 -0500

    Add function to merge logs

--- R/data-processing.R
+++ R/data-processing.R
@@ -33,3 +33,45 @@ merge_tables <- function(files,
     df
 
 }
+
+#' @export
+#' @importFrom purrr map_chr
+#' @importFrom fs path_join dir_delete file_delete
+#' @importFrom tibble tibble
+#' @importFrom dplyr left_join
+#' @importFrom readr read_table
+merge_logs <- function(log_dirs,
+                       job_log,
+                       output_filepath,
+                       remove_logs = FALSE,
+                       remove_job_log = FALSE,
+                       reader = read_file,
+                       writer = write_fst) {
+    read_log <- function(dirs, type) {
+        map_chr(dirs, function(dir) path_join(list(c(dir, type))))
+    }
+
+    stderrs <- map_chr(read_log(log_dirs, "stderr"), reader)
+    stdouts <- map_chr(read_log(log_dirs, "stdout"), writer)
+    seqs <- map_chr(path_join(list(log_dirs, "seq")), reader)
+
+    log_table <- tibble(seq = seqs, stderr = stderrs, stdout = stdouts)
+
+    job_log <- read_table(job_log)
+
+    result <- left_join(job_log, log_table, by = "seq")
+
+    if(!is.null(output_filepath)) {
+        writer(result, output_filepath)
+    }
+
+    if(remove_logs) {
+        dir_delete(log_dirs)
+    }
+
+    if(remove_job_log) {
+        file_delete(job_log)
+    }
+
+    result
+}
