aeeeef52789a7f745aace5dfa222f14f8c9702ba
Aviral Goel <goel.aviral@gmail.com>
2021-03-29 22:23:47 -0400

    Add function to extract corpus and clients

---
+++ R/package.R
@@ -0,0 +1,66 @@
+
+' @export
+' @importFrom utils installed.packages
+#' @importFrom tibble tibble
+#' @importFrom dplyr bind_rows slice pull arrange desc count filter
+#' @importFrom stringr str_trim str_remove str_split
+#' @importFrom fs path_dir dir_create
+#' @importFrom readr write_lines
+select_packages <- function(n = 500,
+                            packages = installed.packages()[,1],
+                            fields = c("Depends", "Imports"),
+                            corpusfile = NULL,
+                            clientfile = NULL) {
+
+    ## filter only from list of supplied packages and fields
+    package_table <- installed.packages()[packages, fields]
+
+    dependencies <- package_table[,fields[1]]
+
+    for(field in fields[-1]) {
+        dependencies <- paste(dependencies, ", ", package_table[, field])
+    }
+
+    dependencies <-
+        dependencies %>%
+        str_split(",") %>%
+        map(function(s) {
+            str_trim(str_remove(s, "\\([a-zA-Z0-9.>=<[:space:]-]*\\)"))
+        })
+
+
+    df <-
+        lapply(1:length(dependencies),
+           function(index) {
+               tibble(client = packages[index],
+                      corpus = dependencies[[index]])
+
+           }) %>%
+    bind_rows() %>%
+    filter(!(corpus %in% c("R", "NA")))
+
+    corpuses <-
+        df %>%
+        count(corpus, name = "count") %>%
+        arrange(desc(count)) %>%
+        slice(1:n) %>%
+        pull(corpus)
+
+    clients <-
+        df %>%
+        filter(corpus %in% corpuses) %>%
+        pull(client) %>%
+        unique()
+
+    if(!is.null(corpusfile)) {
+        dir_create(path_dir(corpusfile))
+        write_lines(corpuses, corpusfile)
+    }
+
+    if(!is.null(clientfile)) {
+        dir_create(path_dir(clientfile))
+        write_lines(clients, clientfile)
+    }
+
+    list(corpus = corpuses, client = clients)
+}
