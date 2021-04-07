93460a6ad808083120af731a577548c0a7b569df
Aviral Goel <goel.aviral@gmail.com>
2021-04-07 00:57:37 -0400

    Fix init method

--- src/init.cpp
+++ src/init.cpp
@@ -8,7 +8,7 @@ static const R_CallMethodDef callMethods[] = {
     {"r_experimentr_run_length_encoding", (DL_FUNC) &r_experimentr_run_length_encoding, 1},
     {NULL, NULL, 0}};
 
-void R_init_strictr(DllInfo* dll) {
+void R_init_experimentr(DllInfo* dll) {
     R_registerRoutines(dll, NULL, callMethods, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
